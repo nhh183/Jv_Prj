c9480c8286a21a150173ac55a45535742b6c4be6
luz183 <darks18325@gmail.com>
2024-04-23 22:47:10 +0700

    first commit

---
+++ UserManagement.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UserManagement{
+
+  public class User{
+    
+
+  }
+
+
+  static Scanner inp = new Scanner(System.in);
+  public static void main(String[] args) {
+
+
+
+
+
+  }
+}
